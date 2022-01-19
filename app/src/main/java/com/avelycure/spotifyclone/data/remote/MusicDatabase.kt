5a8116ff6d7da9dea6de4d73474a8f61e001b035
avelycure <van.emelin2012@yandex.ru>
2022-01-20 00:57:43 +0300

    Initial commit

---
+++ app/src/main/java/com/avelycure/spotifyclone/data/remote/MusicDatabase.kt
@@ -0,0 +1,15 @@
+package com.avelycure.spotifyclone.data.remote
+
+class MusicDatabase {
+
+    /*private val firestore = FirebaseFirestore.getInstance()
+    private val songCollection = firestore.collection(SONG_COLLECTION)
+
+    suspend fun getAllSongs(): List<com.avelycure.spotifyclone.data.entities.Song>{
+        return try {
+            songCollection.get().await().toObjects(com.avelycure.spotifyclone.data.entities.Song::class.java)
+        }catch (e: Exception){
+            emptyList()
+        }
+    }*/
+}
