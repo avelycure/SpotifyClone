7e4efdc7f8ce7f439a67184a2ca3c9de082e0ab5
avelycure <van.emelin2012@yandex.ru>
2022-01-20 13:26:54 +0300

    Music service set up

--- app/src/main/java/com/avelycure/spotifyclone/di/ServiceModule.kt
+++ app/src/main/java/com/avelycure/spotifyclone/di/ServiceModule.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.audio.AudioAttributes
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.DefaultDataSource
 import com.google.android.exoplayer2.util.Util
 import dagger.Module
 import dagger.Provides
@@ -38,5 +38,5 @@ object ServiceModule {
     @Provides
     fun provideDataSourceFactory(
         @ApplicationContext context: Context
-    ) = DefaultDataSourceFactory(context, Util.getUserAgent(context, "Sporify App"))
+    ) = DefaultDataSource.Factory(context)
 }
